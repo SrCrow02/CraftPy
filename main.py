27b29fdb827973253679717f1ce7c4937498f5e8
SrCrow02 <srcrow81194@gmail.com>
2024-01-23 04:27:47 -0300

    Inventario abre e fecha, e a camera fica certinha

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
 from ursina import *
 from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
+inventario_aberto = False
+
 class Voxel(Button):
-    def __init__(self, position=(0,0,0)):
-        super().__init__(parent=scene ,
+    def __init__(self, position=(0, 0, 0)):
+        super().__init__(
+            parent=scene,
             position=position,
             model='cube',
             origin_y=.5,
@@ -18,15 +20,15 @@ class Voxel(Button):
 class Inventory(Entity):
     def __init__(self, width=5, height=5, **kwargs):
         super().__init__(
-            parent = camera.ui,
-            model = Quad(radius=.015),
-            texture = 'white_cube',
-            texture_scale = (width, height),
-            scale = (width*.1, height*.1),
-            origin = (-.5,.5),
-            position = (-.3,.4),
-            color = color.color(0, 0, .1, .9),
-            )
+            parent=camera.ui,
+            model=Quad(radius=.015),
+            texture='white_cube',
+            texture_scale=(width, height),
+            scale=(width * .1, height * .1),
+            origin=(-.5, .5),
+            position=(-.3, .4),
+            color=color.color(0, 0, .1, .9),
+        )
 
         self.width = width
         self.height = height
@@ -50,13 +52,27 @@ def player_fly():
         player.y -= fly_speed * time.dt
 
 def open_inventory():
-    inventory = Inventory()    
+    global inventario_aberto, inventory  
+    inventory = Inventory()
+    
+    inventario_aberto = True
+
+def close_inventory():
+    global inventario_aberto, inventory  
+    if inventory:
+        destroy(inventory)
+        inventory = None
+    inventario_aberto = False
 
 def update():
     player_fly()
-    if held_keys['e']:
+    global inventario_aberto, inventory  
+    if held_keys['e'] and not inventario_aberto:
         open_inventory()
-
+        player.enabled = False
+    elif held_keys['q'] and inventario_aberto:
+        close_inventory()
+        player.enabled = True
 
 player = FirstPersonController()
 player.speed = 10
@@ -65,8 +81,6 @@ player.gravity = 0
 for z in range(15):
     for x in range(15):
         for y in range(2):
-            voxel = Voxel(position=(z,y,x))
-
-      
+            voxel = Voxel(position=(z, y, x))
 
 app.run()
