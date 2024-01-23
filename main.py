8e9d954959c22e80a3c1a4386e481981d75d0978
SrCrow02 <srcrow81194@gmail.com>
2024-01-23 03:22:45 -0300

    Adicionei o objeto do inventario

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Ursina()
 
 class Voxel(Button):
     def __init__(self, position=(0,0,0)):
-        super().__init__(parent=scene,
+        super().__init__(parent=scene ,
             position=position,
             model='cube',
             origin_y=.5,
@@ -15,11 +15,24 @@ class Voxel(Button):
             highlight_color=color.blue,
         )
 
-for z in range(15):
-    for x in range(15):
-        for y in range(2):
-            voxel = Voxel(position=(z,y,x))
+class Inventory(Entity):
+    def __init__(self, width=5, height=5, **kwargs):
+        super().__init__(
+            parent = camera.ui,
+            model = Quad(radius=.015),
+            texture = 'white_cube',
+            texture_scale = (width, height),
+            scale = (width*.1, height*.1),
+            origin = (-.5,.5),
+            position = (-.3,.4),
+            color = color.color(0, 0, .1, .9),
+            )
 
+        self.width = width
+        self.height = height
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
 def input(key):
     if key == 'left mouse down':
@@ -29,17 +42,31 @@ def input(key):
     if key == 'right mouse down' and mouse.hovered_entity:
         destroy(mouse.hovered_entity)
 
-
-def update():
+def player_fly():
     fly_speed = 15
     if held_keys['space']:
         player.y += fly_speed * time.dt
     if held_keys['control']:
         player.y -= fly_speed * time.dt
 
+def open_inventory():
+    inventory = Inventory()    
+
+def update():
+    player_fly()
+    if held_keys['e']:
+        open_inventory()
+
 
 player = FirstPersonController()
 player.speed = 10
 player.gravity = 0
+
+for z in range(15):
+    for x in range(15):
+        for y in range(2):
+            voxel = Voxel(position=(z,y,x))
+
+      
 
 app.run()
